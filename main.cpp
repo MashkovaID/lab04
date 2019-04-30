3ac9704eabb7e634a533466058b5fbe4aea1bb5a
MashkovaID <u211-14@public.mpei.local>
2019-04-30 13:54:59 +0300

    build:заготовка программы

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    void print_in_hex(uint8_t byte);
+void print_in_hex(const void* data, size_t size);
+void print_in_binary(uint8_t byte);
+void print_in_binary(const void* data, size_t size);
+}
